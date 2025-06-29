b31321ca372585be5caced3a2957c39a1b2b27c6
CerveraMarco <cerverapootfernan@gmail.com>
2025-06-29 00:57:59 -0600

    mi primer test wiii

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,40 +1,22 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-});
-
-test('get started link 3', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-}); 
-
 test('Busqueda', async ({ page }) => {
   await page.goto('https://www.mercadolibre.com.mx/');
 
-  // Fill the elemente
+  // Buscar
   await page.locator('#cb1-edit').fill('Iphone');
-
-  // Search
   await page.keyboard.press('Enter');
 
-  await expect(page.locator('.ui-search-layout')).toBeVisible;
-}); 
+  // Esperar que aparezcan resultados
+  await expect(page.locator('.ui-search-layout')).toBeVisible();
+
+  // Extraer títulos
+  const titles = await page.locator('.ui-search-layout h3').allInnerTexts();
+
+  for (let title of titles) {
+    console.log(title);
+  }
+
+  console.log('Encontramos', titles.length);
+});
+
